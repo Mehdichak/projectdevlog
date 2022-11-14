3429d824da7152dab5b3c8083c6eae775075cd52
NicolasLeroy1 <nicolas.leroy@etu.umontpellier.fr>
2022-11-14 13:57:48 +0100

    Update PredictionDataLoading.py

--- Prediction/PredictionDataLoading.py
+++ Prediction/PredictionDataLoading.py
@@ -1,9 +1,18 @@
 import pandas as pd
+import matplotlib as plt
+
+# On charge le fichier csv en tant que dataframe
 filepath="predictiondata.csv"
 table = pd.read_csv(filepath,sep=";")
-table.info()
+
+# On enlève les données non utiles
 table=table.drop(columns=["Périmètre","Nature","Date","Heure","Ech. comm. Angleterre (MW)","Ech. comm. Espagne (MW)","Ech. comm. Italie (MW)","Ech. comm. Suisse (MW)","Ech. comm. Allemagne-Belgique (MW)","Stockage batterie (MW)","Déstockage batterie (MW)","Eolien terrestre (MW)","Eolien offshore (MW)"])
-table.info()
+
+# On enlève les lignes qui ont des données manquantes
 table = table.dropna()
-table.info()
+
+# On selectionne comme index la donnée temporelle
+table = table.set_index("Date - Heure")
 print(table.head())
+
+print(table.info())
