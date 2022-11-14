4caf2aabe3d6ecff240b5eb147139747094c0b3b
NicolasLeroy1 <nicolas.leroy@etu.umontpellier.fr>
2022-11-14 22:01:48 +0100

    Update PredictionAnalyse.py

--- PredictionAnalyse/PredictionAnalyse.py
+++ PredictionAnalyse/PredictionAnalyse.py
@@ -6,12 +6,14 @@ filepath="predictiondata.csv"
 table = pd.read_csv(filepath,sep=";")
 
 # On enlève les données non utiles
-table=table.drop(columns=["Périmètre","Nature","Date","Heure","Ech. comm. Angleterre (MW)","Ech. comm. Espagne (MW)","Ech. comm. Italie (MW)","Ech. comm. Suisse (MW)","Ech. comm. Allemagne-Belgique (MW)","Stockage batterie (MW)","Déstockage batterie (MW)","Eolien terrestre (MW)","Eolien offshore (MW)"])
+table=table.drop(columns=["Périmètre","Nature","Ech. comm. Angleterre (MW)","Ech. comm. Espagne (MW)","Ech. comm. Italie (MW)","Ech. comm. Suisse (MW)","Ech. comm. Allemagne-Belgique (MW)","Stockage batterie (MW)","Déstockage batterie (MW)","Eolien terrestre (MW)","Eolien offshore (MW)"])
 
 # On enlève les lignes qui ont des données manquantes
 table = table.dropna()
 
 # On selectionne comme index la donnée temporelle
+table = table.set_index("Date - Heure")
 print(table.head())
-
 print(table.info())
+
+plt
