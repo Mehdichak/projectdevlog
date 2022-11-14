fe6894a877682736dd5d673d894acaf27f4a6b8c
NicolasLeroy1 <nicolas.leroy@etu.umontpellier.fr>
2022-11-14 12:35:39 +0100

    dataclean

--- Prediction/PredictionDataLoading.py
+++ Prediction/PredictionDataLoading.py
@@ -1,3 +1,9 @@
 import pandas as pd
-filename=eco2mix-national-tr.csv
-table = pd.read_csv(filename,sep=";")
+filepath="predictiondata.csv"
+table = pd.read_csv(filepath,sep=";")
+table.info()
+table=table.drop(columns=["Périmètre","Nature","Date","Heure","Ech. comm. Angleterre (MW)","Ech. comm. Espagne (MW)","Ech. comm. Italie (MW)","Ech. comm. Suisse (MW)","Ech. comm. Allemagne-Belgique (MW)","Stockage batterie (MW)","Déstockage batterie (MW)","Eolien terrestre (MW)","Eolien offshore (MW)"])
+table.info()
+table = table.dropna()
+table.info()
+print(table.head())
