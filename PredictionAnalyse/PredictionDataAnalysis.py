3ecab3671d101e5375401d0e8dbf2eaf8e6c15a9
NicolasLeroy1 <nicolas.leroy@etu.umontpellier.fr>
2022-12-03 21:46:51 +0100

    Update PredictionDataAnalysis.py

--- PredictionAnalyse/PredictionDataAnalysis.py
+++ PredictionAnalyse/PredictionDataAnalysis.py
@@ -8,21 +8,24 @@ longterm = pd.read_csv("LongTermCleanData.csv", sep=";")
 longterm["Date - Heure"] = pd.to_datetime(longterm["Date - Heure"])
 longterm = longterm.set_index("Date - Heure")
 
-# On regarde l'erreur des prédictions naives et naives saisonieres
+# On regarde le décalage des prédictions naives et naives-saisonieres
 NlongtermError = longterm - longterm.shift(15,freq="T")
 NlongtermError = NlongtermError.dropna()
 SNlongtermError = longterm - longterm.shift(365,freq="D")
 SNlongtermError = SNlongtermError.dropna()
 
+#Et on calcule l'erreur quadratique moyennes associées
+print((NlongtermError["Consommation (MW)"]*NlongtermError["Consommation (MW)"]).mean())
+print((SNlongtermError["Consommation (MW)"]*SNlongtermError["Consommation (MW)"]).mean())
+
+# On regarde une décomposition saisonière multiplicative de notre série temporelle principale
 decomposition = seasonal_decompose(longterm["Consommation (MW)"], model='multiplicative', period=365*24*4)  
 fig = decomposition.plot()
 fig.set_size_inches(14, 7)
 plt.show()
-decomposition = seasonal_decompose(longterm["Consommation (MW)"], model='multiplicative', period=7*24*4)  
+
+# On regarde une décomposition seasonière additive de notre série temporelle principale
+decomposition = seasonal_decompose(longterm["Consommation (MW)"], model='additive', period=365*24*4)  
 fig = decomposition.plot()
 fig.set_size_inches(14, 7)
 plt.show()
-
-
-print((NlongtermError["Consommation (MW)"]*NlongtermError["Consommation (MW)"]).mean())
-print((SNlongtermError["Consommation (MW)"]*SNlongtermError["Consommation (MW)"]).mean())
