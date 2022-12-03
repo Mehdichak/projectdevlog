e995bf4ab4971fc841deef5c7ab68b6d81c9c1b1
NicolasLeroy1 <nicolas.leroy@etu.umontpellier.fr>
2022-12-03 21:44:21 +0100

    Update PredictionDataCleaning.py

--- PredictionAnalyse/PredictionDataCleaning.py
+++ PredictionAnalyse/PredictionDataCleaning.py
@@ -1,21 +1,16 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import datetime
-import numpy as np
 
 # On charge le fichier csv en tant que dataframe
 filepath="predictiondata.csv"
 filepath2="LongTermData.csv"
 table = pd.read_csv(filepath,sep=";")
 table2 = pd.read_csv(filepath2,sep=";")
-print(table2.info())
-
 
 # On enlève les données non utiles, on fait en sorte d'avoir les memes colonnes sur les deux tables
 table=table.drop(columns=["Prévision J (MW)","Prévision J-1 (MW)","Périmètre","Nature","Ech. comm. Angleterre (MW)","Ech. comm. Espagne (MW)","Ech. comm. Italie (MW)","Ech. comm. Suisse (MW)","Ech. comm. Allemagne-Belgique (MW)","Stockage batterie (MW)","Déstockage batterie (MW)","Eolien terrestre (MW)","Eolien offshore (MW)"])
 table2=table2.drop(columns=["Prévision J (MW)","Périmètre","Prévision J-1 (MW)","Nature","Ech. comm. Angleterre (MW)","Ech. comm. Espagne (MW)","Ech. comm. Italie (MW)","Ech. comm. Suisse (MW)","Ech. comm. Allemagne-Belgique (MW)"])
 table = table.dropna()
-
 
 # On construit un index temporel adapté
 table["Date - Heure"] = pd.to_datetime(table["Date"] + ' ' +table["Heure"])
