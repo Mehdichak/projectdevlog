d1e0a5ef4eb7783f92a914a9048d77af08ef7f1c
NicolasLeroy1 <nicolas.leroy@etu.umontpellier.fr>
2022-12-04 13:20:56 +0100

    Update TestPrediction.py

--- PredictionAnalyse/TestPrediction.py
+++ PredictionAnalyse/TestPrediction.py
@@ -3,13 +3,13 @@ import datetime
 from statsmodels.tsa.holtwinters import ExponentialSmoothing, SimpleExpSmoothing, Holt
 import matplotlib.pyplot  as plt
 
-Data = pd.read_csv("LongTermCleanData.csv", sep=";")
+Data = pd.read_csv("PredictionCleanData.csv", sep=";")
 Data["Date - Heure"] = pd.to_datetime(Data["Date - Heure"])
 Data = Data.set_index("Date - Heure")
 
 
-DataToFit = Data["Consommation (MW)"]['2020-01-01':'2022-01-01'].resample("30T").mean()
-DataToPredict = Data["Consommation (MW)"]['2022-01-01':'2022-01-27']
+DataToFit = Data["Consommation (MW)"]['2020-05-31':'2022-05-31'].resample("30T").mean()
+DataToPredict = Data["Consommation (MW)"]['2022-06-01':'2022-06-03']
 
 
 
@@ -24,5 +24,8 @@ Fit = ExponentialSmoothing(
 ).fit()
 
 Fit.fittedvalues.plot(style='--', color='red', label='train')
-DataToPredict.plot(style='--', color='green', label='test')
+plt.show()
+
+DataToPredict.plot(style='--', color='blue', label='test')
+Fit.Forecast(2*24*2).plot(style='--', color='yellow', label='prediction')
 plt.show()
