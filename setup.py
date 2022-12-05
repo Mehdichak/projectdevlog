c100010e7f25a6b81a93f9f864bc87e5b15f8c76
NicolasLeroy1 <nicolas.leroy@etu.umontpellier.fr>
2022-12-06 00:11:45 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
-from Projet import __version__ as current_version
+from predivis import __version__ as current_version
 
 setup(
-  name='Projet',
+  name='predivis',
   version=current_version,
   description='Visualization and prediction of French Electrical Consumption',
   url='https://github.com/Mehdichak/projectdevlog.git',
   author='Leroy Nicolas, Chakroun Mohamed Mehdi, Axel de Montgolfier',
   author_email='nicolas.leroy@etu.umontpellier.fr, axel.de-montgolfier@etu.umontpellier.fr, mohamed-mehdi.chakroun@etu.umontpellier.fr ',
   license='MIT',
-  packages=['Projet','Projet.Data_visu', 'Projet.prediction'],
-  zip_safe=False
+  packages=['predivis','predivis.visu', 'predivis.prediction'],
+  zip_safe=False,
+  package_data={'predivis':["Data/*"]}
 )
