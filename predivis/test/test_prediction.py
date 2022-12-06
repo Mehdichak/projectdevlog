bd58c5bbc0522e800701c3042a48efc30695e412
NicolasLeroy1 <nicolas.leroy@etu.umontpellier.fr>
2022-12-06 22:48:08 +0100

    upgrade test_prediction

--- predivis/test/test_prediction.py
+++ predivis/test/test_prediction.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 def test_dataload():
     assert predivis.prediction.dataloading() == 1
-def test_predict_for_day():
+def test_Methode_Prophet():
     assert isinstance(predivis.prediction.predict_for_day(filepath="PredictionCleanData.csv",filepath_out="prediction.csv", date_initiale='2022-11-15', methode='Prophet', source_conso="Consommation (MW)", date_prediction='2022-12-08', save_model=False, load_model=False) , pd.DataFrame)
+def test_methodeHoltWinters():
     assert isinstance(predivis.prediction.predict_for_day(methode="Holt_Winters",date_initiale='2022-11-15'), pd.DataFrame)
 
