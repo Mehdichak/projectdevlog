271851e984272dcbd4d46bb50574e5d647d261d6
NicolasLeroy1 <nicolas.leroy@etu.umontpellier.fr>
2022-12-06 19:10:30 +0100

    test upgrade

--- predivis/visu/__init__.py
+++ predivis/visu/__init__.py
@@ -1,4 +1,4 @@
-from .data_visu import Visu_multiple as visu_multiple
-from .min_max import min_max,fct_min_max
-from .visu_commune import Visu_Final as visu_final
-from .visu_complet import Visu_Unique as visu_unique
+from .datavisu import Visumultiple as visumultiple
+from .minmax import min_max,fct_min_max
+from .visucommune import Visu_Final as visu_final
+from .visucomplet import Visu_Unique as visu_unique
--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import predivis
 
-predivis.prediction.dataloading()
-predivis.prediction.predict_for_day()
+predivis.prediction.dataloading(finalfilepath="PredictionCleanData.csv")
+predivis.prediction.predict_for_day(filepath="PredictionCleanData.csv",filepath_out="prediction.csv", date_initiale='2021-12-07', methode='Prophet', source_conso="Consommation (MW)", date_prediction='2022-12-08', save_model=False, load_model=False)
 
-predivis.visu.visu_multiple()
+
 predivis.visu.visu_final()
-predivis.visu.visu_unique()
-predivis.visu.visu_multiple()
+#predivis.visu.visu_unique()
+#predivis.visu.visu_multiple()
 
 
 
