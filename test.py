2e0da71859d752dd6a77ddd011eb3e7694fa9fd5
NicolasLeroy1 <nicolas.leroy@etu.umontpellier.fr>
2022-12-06 22:32:56 +0100

    gitignore

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import predivis
 
-predivis.prediction.dataloading(finalfilepath="PredictionCleanData.csv")
+#predivis.prediction.dataloading(finalfilepath="PredictionCleanData.csv")
 #predivis.prediction.predict_for_day(filepath="PredictionCleanData.csv",filepath_out="prediction.csv", date_initiale='2021-12-07', methode='Prophet', source_conso="Consommation (MW)", date_prediction='2022-12-08', save_model=False, load_model=False)
-
+predivis.prediction.predict_for_day(methode="Holt_Winters",date_initiale='2022-11-15')
 
 #predivis.visu.visu_final()
 #predivis.visu.visu_unique()
